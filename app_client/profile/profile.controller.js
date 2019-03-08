b6886c57f7f39acb30a67e73a69aa82e16c71c4a
Rajan <rajj108@gmail.com>
2019-03-08 08:09:03 -0800

    add functionality to edit buttons

--- app_client/profile/profile.controller.js
+++ app_client/profile/profile.controller.js
@@ -1,12 +1,24 @@
 angular.module('RAuthApp')
-  .controller('profileCtrl', ['$location', 'meanData', function ($location, meanData) {
-    var vm = this;
-    vm.user = {};
-    
-    meanData.getProfile()
-      .then(function (data) {
-        vm.user = data.data;
-      }, function (data) {
-        console.log(data);
-    });
+    .controller('profileCtrl', ['$location', 'meanData', function ($location, meanData) {
+        var vm = this;
+        vm.user = {};
+
+        meanData.getProfile()
+            .then(function (data) {
+                vm.user = data.data;
+            }, function (data) {
+                console.log(data);
+            });
+
+
+        vm.edit = function (field) {
+            if (field === 'name') {
+                vm.editName = true;
+                vm.editEmail = false;
+            } else {
+                vm.editName = false;
+                vm.editEmail = true;
+            }
+
+        }
 }]);
